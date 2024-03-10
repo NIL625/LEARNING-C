b36d8f1f0c4c78a45aa84e1f79cd87d608e05b85
Shilajit Khan <shilajit.khan008@gmail.com>
2024-03-10 13:18:08 +0530

    my first practice code

---
+++ coursechef/practice/PCL01_problemsSTART01.c
@@ -0,0 +1,12 @@
+// Number Mirror
+// Write a program that takes a number N as the input, and prints it to the output.
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int N;
+	scanf("%d", &N);
+	printf("%d", N);
+return 0;
+}

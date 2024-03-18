b1bad3b7af80db2fcd09a03cfaac10e851597e7e
Shilajit Khan <shilajit.khan008@gmail.com>
2024-03-18 23:44:57 +0530

    hacker rank 1

---
+++ hackerrank/playing-with-characters.c
@@ -0,0 +1,11 @@
+//You have to print the character, ch, in the first line. Then print  in next line s. In the last line print the sentence,sen.
+#include<stdio.h>
+int main()
+ { char ch,s[100],sen[200];
+  scanf( "%c",&ch); scanf("\n");
+   scanf("%s",s); scanf("\n"); 
+   gets(sen); scanf("\n"); 
+   printf("%c\n",ch);
+    printf("%s\n",s); 
+    printf("%s\n",sen);
+}
